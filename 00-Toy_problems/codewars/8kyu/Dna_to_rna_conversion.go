2f870de98bb464158e432fd09a3b5c447e5b11f7
Seunghoon Ko <imskojs@gmail.com>
2019-07-09 07:24:37 +0900

    [toy] Dna to rna conversion [best 2]

--- 00-Toy_problems/codewars/8kyu/Dna_to_rna_conversion.go
+++ 00-Toy_problems/codewars/8kyu/Dna_to_rna_conversion.go
@@ -18,3 +18,14 @@ func DNAtoRNA(dna string) string {
 func DNAtoRNA1(dna string) string {
 	return strings.Replace(dna, "T", "U", -1)
 }
+
+func DNAtoRNA2(dna string) string {
+	var response string
+	for _, letter := range dna {
+		if letter == 'T' {
+			letter = 'U'
+		}
+		response += string(letter)
+	}
+	return response
+}
